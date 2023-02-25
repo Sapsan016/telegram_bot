6a80e0e9100091d5566da67738e166152b1a2cca
Mikhail Gorbunov <mixalich84@yandex.ru>
2023-02-26 00:10:05 +0300

    Initial commit

---
+++ src/test/java/com/github/sapsan016/telegrambot/TelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sapsan016.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
